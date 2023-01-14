6674947829e83c2ccb5a353824990f38c65e841d
PavKot <36005871+PavKot@users.noreply.github.com>
2023-01-15 00:29:40 +0200

    Add files via upload

---
+++ awarezone-last/eng/burgerMenu.js
@@ -0,0 +1,14 @@
+// ADAPTIVE BURGER MENU
+
+
+window.addEventListener('scroll', function(){
+    const header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY>0);
+})
+
+function toggleMenu(){
+    const menuToggle = document.querySelector('.menuToggle');
+    const navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
